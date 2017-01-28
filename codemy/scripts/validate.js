8213d9a67b65e680946f1f08358d46d0a8ea1b6a
Mario Sanchez Carrion <mariosc@gmail.com>
2017-01-28 14:20:58 -0500

    add fixed validator

--- codemy/scripts/validate.js
+++ codemy/scripts/validate.js
@@ -7,6 +7,15 @@ function validateText() {
  };
 }
 
+function validatePassword() {
+  if(document.querySelector('input[type="password"]').value.trim() !== ""){
+    document.querySelector('input[type="password"]').style["borderBottom"] = "3px solid #1de71d";
+  }
+ else {
+   document.querySelector('input[type="text"]').style["borderBottom"] = "3px solid #F00";
+ };
+}
+
 function validateEmail() {
   
   if(document.querySelector('input[type="email"]').value.includes("@") && document.querySelector('input[type="email"]').value.includes(".")){
