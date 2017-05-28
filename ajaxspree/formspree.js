9d5e7e3d51a47a900462b9afd695a3cee566771f
mariobox <mariosc@gmail.com>
2017-05-28 16:21:10 -0400

    modify script

--- ajaxspree/formspree.js
+++ ajaxspree/formspree.js
@@ -11,25 +11,16 @@ $contactForm.submit(function(e) {
 		data: $(this).serialize(),
 		dataType: 'json',
 		beforeSend: function() {
-			//$contactForm.append('<div class="alert alert--loading">Sending message…</div>');
-			$submit.attr('disabled', true).val('Sending message…');
+			$contactForm.append('<div class="alert alert--loading">Sending message…</div>');
 		},
 		success: function(data) {
-			//$contactForm.append('<div class="alert alert--success">Message sent!</div>');
-			$submit.val('Message sent!');
-			setTimeout(function() {
-				//$('.alert--success').remove();
-				$submit.attr('disabled', false).val(defaultSubmitText);
-			}, 5000);
+			$contactForm.find('.alert--loading').hide();
+			$contactForm.append('<div class="alert alert--success">Message sent!</div>');
 		},
 		error: function(err) {
-			//$contactForm.find('.alert--loading').hide();
-			//$contactForm.append('<div class="alert alert--error">Ops, there was an error.</div>');
-			$submit.val('Ops, there was an error.');
-			setTimeout(function() {
-				//$('.alert--error').remove();
-				$submit.attr('disabled', false).val(defaultSubmitText);
-			}, 5000);
+			$contactForm.find('.alert--loading').hide();
+			$contactForm.append('<div class="alert alert--error">Ops, there was an error.</div>');
+			
 		}
 	});
 });
